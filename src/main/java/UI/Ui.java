f9997641f2df09040bec63a9d57bf4a6f398bb5c
aroo-dev <axxrxxo@gmail.com>
2024-05-21 18:12:20 +0200

    Initial commit

---
+++ src/main/java/UI/Ui.java
@@ -0,0 +1,13 @@
+package UI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieNr3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieNr3Application.class, args);
+    }
+
+}
---
+++ src/test/java/UI/ZadanieNr3ApplicationTests.java
@@ -0,0 +1,13 @@
+package UI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieNr3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
